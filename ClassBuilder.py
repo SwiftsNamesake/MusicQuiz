8f12ca9b7c89c982e41b7236370e44650802b5f5
Jonatan H Sundqvist <jonatanhsundqvist@gmail.com>
2014-10-19 21:40:45 +0200

    1) Fixed issues with spaces appearing in variable names (ClassBuilder.py)

--- ClassBuilder.py
+++ ClassBuilder.py
@@ -15,6 +15,7 @@
 
 from time import localtime, strftime
 from os.path import basename, splitext
+from os import listdir
 
 
 template = '''
@@ -67,20 +68,20 @@ def createClass(name, package, author, resources):
 	
 	with open('%s.java' % name, 'r', encoding='utf-8') as f:
 		# TODO: Align equal signs
+		# TODO: Relative paths
 		return template.format(**({
 			'header': '\n'.join(' * %s' % line for line in ('%s.java' % name, 'Contains resources for %s' % package, '', author, strftime('%a %c', localtime()) )),
 			'package': package,
 			'name': name,
- 			'media': '\n\t'.join('public static final Media %s = Utilities.loadSound(%s);' % (splitext(basename(res))[0], res) for res in resources),
- 			#'\n\t'.join('public static final String %s = \'%s\';' % tuple(line[:-1].split('=')) for line in f if '=' in line)),
-			'IDs': '\n\t'.join('public static final int %s = %d;' % (splitext(basename(name))[0].upper(), n) for n, name in enumerate(resources)),
+ 			'media': '\n\t'.join('public static final Media %s = Utilities.loadSound(\'%s\');' % (splitext(basename(res))[0].replace(' ', '_'), res) for res in resources),
+			'IDs': '\n\t'.join('public static final int %s = %d;' % (splitext(basename(name))[0].upper().replace(' ', '_'), n) for n, name in enumerate(resources)),
 			'byID': '\n\t\t\t'.join('case %s: return %s;' % (line, line.upper()) for line in map(lambda res: splitext(basename(res))[0], resources))
 		}))
 
 
 def main():
 	# print(createClass('sv_strings.txt'))
-	print(createClass('R', 'MusicQuiz', 'Jonatan H Sundqvist', ('resources/ding.wav', 'resources/strangled.wav')))
+	print(createClass('R', 'MusicQuiz', 'Jonatan H Sundqvist', tuple(filter(lambda path: splitext(path)[1].lower() in ('.wav', '.mp3'), listdir('resources')))))
 
 
 if __name__ == '__main__':
